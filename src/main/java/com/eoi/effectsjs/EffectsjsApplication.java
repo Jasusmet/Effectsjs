8c89a42376fb9e3d372b7c4faf060dc0733a054d
Jesús Clemente Pérez <jmclementeperez@gmail.com>
2024-07-19 10:42:06 +0200

    Commit Inicial

---
+++ src/main/java/com/eoi/effectsjs/EffectsjsApplication.java
@@ -0,0 +1,13 @@
+package com.eoi.effectsjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectsjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffectsjsApplication.class, args);
+	}
+
+}
